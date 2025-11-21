6947697729121642e00e7320e9d8797cfd7197ae
Raquel Leite <raquel.rleite@outlook.com>
2025-11-21 12:34:50 -0300

    FEAT: project created with Spring Boot

---
+++ src/test/java/brcom/banksecure/app/BanksecureApplicationTests.java
@@ -0,0 +1,13 @@
+package brcom.banksecure.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
