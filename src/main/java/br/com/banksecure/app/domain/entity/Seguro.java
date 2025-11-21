cd1750942211976116ef80c5b453bc65174fd167
Raquel Leite <raquel.rleite@outlook.com>
2025-11-21 14:06:42 -0300

    FEAT: add domain entities for Cliente, Funcionario, Seguro and Apolice

---
+++ src/main/java/br/com/banksecure/app/domain/entity/Seguro.java
@@ -0,0 +1,35 @@
+package br.com.banksecure.app.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "seguro")
+public class Seguro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Título é obrigatório.")
+    private String titulo;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Cobertura Mínima é obrigatória.")
+    private String coberturaMinima;
+
+    @Column(nullable = false)
+    @NotNull(message = "O prêmio base é obrigatório.")
+    @Positive(message = "O valor do prêmio deve ser maior que zero.")
+    private BigDecimal valorPremioBase;
+}
