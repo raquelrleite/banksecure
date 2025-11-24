b814d0a3c6e82a124ead15bccc1324abaeb83a24
Raquel Leite <raquel.rleite@outlook.com>
2025-11-24 15:50:32 -0300

    BUILD: add MapStruct dependency and replace manual mappers

--- src/main/java/br/com/banksecure/app/mapper/FuncionarioMapper.java
+++ src/main/java/br/com/banksecure/app/mapper/FuncionarioMapper.java
@@ -2,35 +2,13 @@ package br.com.banksecure.app.mapper;
 
 import br.com.banksecure.app.domain.entity.Funcionario;
 import br.com.banksecure.app.dto.request.FuncionarioRequest;
-import br.com.banksecure.app.dto.request.LoginRequest;
 import br.com.banksecure.app.dto.response.FuncionarioResponse;
 import br.com.banksecure.app.dto.response.LoginResponse;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class FuncionarioMapper {
-
-    public Funcionario converterParaEntity(FuncionarioRequest request) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome(request.nome());
-        funcionario.setCargo(request.cargo());
-        funcionario.setUsername(request.username());
-        funcionario.setPassword(request.password());
-        return funcionario;
-    }
-
-    public FuncionarioResponse converterParaResponse(Funcionario funcionario) {
-        return new FuncionarioResponse(
-                funcionario.getId(),
-                funcionario.getNome(),
-                funcionario.getCargo(),
-                funcionario.getUsername()
-        );
-    }
-
-    public LoginResponse converterParaLogin(Funcionario funcionario) {
-        return new LoginResponse(
-                funcionario.getId()
-        );
-    }
+@Mapper(componentModel = "spring")
+public interface FuncionarioMapper {
+    Funcionario converterParaEntity(FuncionarioRequest request);
+    FuncionarioResponse converterParaResponse(Funcionario funcionario);
+    LoginResponse converterParaLogin(Funcionario funcionario);
 }
