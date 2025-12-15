dbfb9393363139bbf1cc619b5d682e23730c4b31
Raquel Leite <raquel.rleite@outlook.com>
2025-12-15 18:15:58 -0300

    add builder and TipoSeguroeBem

--- src/main/java/br/com/banksecure/app/dto/response/BemResponse.java
+++ src/main/java/br/com/banksecure/app/dto/response/BemResponse.java
@@ -1,9 +1,13 @@
 package br.com.banksecure.app.dto.response;
 
+import br.com.banksecure.app.enums.TipoSeguroeBem;
+import lombok.Builder;
+
+@Builder
 public record BemResponse(
         Long id,
         Long clienteId,
-        String tipo,
+        TipoSeguroeBem tipo,
         String descricao
 ) {
 }
